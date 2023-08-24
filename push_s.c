360747c9d37e1cc35f5dfc7a8bed00f37b8d5e8f
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-08-24 17:46:19 +0200

    add push function

---
+++ push_s.c
@@ -0,0 +1,32 @@
+#include "monty.h"
+
+/**
+* push_stack - Pushes a new element onto the stack.
+*
+* @top: A pointer to the top of the stack.
+* @val: The value to be pushed onto the stack.
+*/
+
+void push_stack(stack_t **top, int val)
+{
+	stack_t *new_n;
+
+	new_n = malloc(sizeof(stack_t));
+	if (new_n == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_n->n = val;
+	new_n->prev = NULL;
+	new_n->next = NULL;
+
+	if (*top != NULL)
+	{
+		new_n->next = *top;
+		(*top)->prev = new_n;
+	}
+
+	*top = new_n;
+}
